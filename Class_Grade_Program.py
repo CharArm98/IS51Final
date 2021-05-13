fec2209fd4125a84604acece13bed03ada3ad775
CharArm98 <78181235+CharArm98@users.noreply.github.com>
2021-05-13 16:58:02 -0700

    finished structured-english

---
+++ Class_Grade_Program.py
@@ -0,0 +1,13 @@
+
+
+
+""" Structured English 
+start
+create a list to store 24 numbers (float)
+capture users input 24 times for their grades
+each time we capture the user's input, we append the number to the list 
+sort the list ascending, then splice the item starting at index 1
+displaying the number of grades, the average, and the percentage of grades above the average
+output a messsage to the user
+
+"""
