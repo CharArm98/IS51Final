5518cc37a46479a2f3d6e4249d181ca0cb994042
CharArm98 <78181235+CharArm98@users.noreply.github.com>
2021-05-13 17:05:37 -0700

    finished exam-end

--- Class_Grade_Program.py
+++ Class_Grade_Program.py
@@ -26,3 +26,24 @@ Enter one of five grades: 90
 Average grade : 91.33
 
 """
+
+def main():
+    ## Display final grades
+    Finaldict = createDictFromFile("Final.txt")
+    displayInfoAboutFinalGrades(finalDict)
+
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+
+
+for i in range( len(grades)):
+        grades [i] = int(grades [i])
+average = sum (grades) / len (grades)
+num = 0 # number of grades above average
+for grade in grades:
+    if grade > average:
+         num +=1
+print("Number of grades:", len (grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2f}%" .format(100 * num / len(grades)))
