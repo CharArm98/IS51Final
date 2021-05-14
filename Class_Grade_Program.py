4c66e2a943af714ff679a92c08f350388ebe7580
CharArm98 <78181235+CharArm98@users.noreply.github.com>
2021-05-13 17:02:10 -0700

    finished pseudocode

--- Class_Grade_Program.py
+++ Class_Grade_Program.py
@@ -11,3 +11,18 @@ displaying the number of grades, the average, and the percentage of grades above
 output a messsage to the user
 
 """
+
+""" Pseudocode
+Number of grades: 24
+Average grade: 83.25
+Percentage of grade: 83.25
+Average: 54.17 percent 
+
+Enter one of five grades: 84
+Enter one of five grades: 96
+Enter one of five grades: 88
+Enter one of five grades: 77
+Enter one of five grades: 90
+Average grade : 91.33
+
+"""
